39d6950260be7bec642be7d70ac3be0a150ae620
Shweta Agrawala <agrawalashweta@gmail.com>
2025-06-26 12:00:56 +0530

    small change in 32 bit adder

--- 32bitadder/32bitadder.v
+++ 32bitadder/32bitadder.v
@@ -1,8 +1,8 @@
-module 32bitadder(  input[31:0] A,
+module _32bitadder(  input[31:0] A,
                     input[31:0] B,
                     input Cin,
                     output[31:0] Sum,
                     output Cout);
-                    
+
                     assign {Cout,Sum}=A+B+Cin;
 endmodule
